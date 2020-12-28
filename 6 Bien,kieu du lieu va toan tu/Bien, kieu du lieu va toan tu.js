9ca9eb81b6c442182cb1f5fe55806ae84d7eeb98
unknown <you@example.com>
2020-12-28 17:32:36 +0700

    BaitaocuaDOISYTHANG

--- 6 Bien,kieu du lieu va toan tu/Bien, kieu du lieu va toan tu.js
+++ 6 Bien,kieu du lieu va toan tu/Bien, kieu du lieu va toan tu.js
@@ -1,36 +1,40 @@
 function diemtb() {
-    var a,b,c, d;
+    var a, b, c, d;
     a = parseInt(document.getElementById("diemsinh").value);
     b = parseInt(document.getElementById("diemly").value);
     c = parseInt(document.getElementById("diemhoa").value);
-    d = (a +b+c)/3;
-document.getElementById("diemtb").innerText = d;
+    d = (a + b + c) / 3;
+    document.getElementById("diemtb").innerText = d;
 }
+
 function diemtong3mon() {
-    var q,w,e,r;
+    var q, w, e, r;
     q = parseInt(document.getElementById("diemsinh").value);
     w = parseInt(document.getElementById("diemly").value);
     e = parseInt(document.getElementById("diemhoa").value);
-    r = q +w+e;
-    document.getElementById("diemtong3mon").innerText = e;
+    r = q + w + e;
+    document.getElementById("diemtong3mon").innerText = r;
 }
+
 function chuyendoisangf() {
     var x, y;
     x = parseInt(document.getElementById("nhietdoc").value);
-    y = (x* 1.8) + 32;
+    y = (x * 1.8) + 32;
     document.getElementById("chuyendoisangf").innerText = y;
 
 }
+
 function dientichhinhtron() {
-    var k,h;
+    var k, h;
     k = parseInt(document.getElementById("bankinhhinhtron").value);
 
-h = k*k*3.14;
-document.getElementById("dientichhinhtron").innerText = h;
+    h = k * k * 3.14;
+    document.getElementById("dientichhinhtron").innerText = h;
 }
+
 function chuvihinhtron() {
-var l,j;
-l = parseInt(document.getElementById("duongkinhhinhtron").value);
-    j = l*3.14;
-    document.getElementById("chuvihinhtron").innerText= j;
+    var l, j;
+    l = parseInt(document.getElementById("duongkinhhinhtron").value);
+    j = l * 3.14;
+    document.getElementById("chuvihinhtron").innerText = j;
 }
